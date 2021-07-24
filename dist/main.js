68e62bb1e2e8ae0aab7c710f897400ac1a13df20
siIas <kaczkowski.lukasz@outlook.com>
2021-07-24 21:10:16 +0200

    adding new project titles to localStorage

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,32 @@
-console.log("hello, test");
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("const LOCAL_STORAGE_LIST_KEY = 'task.lists';\r\nlet lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];\r\n\r\n// New projects list\r\nconst projectsList = document.querySelector('#projects-list');\r\n// Add/delete project - functionality\r\nconst addProjectBtn = document.querySelector('#button-add-project');\r\nconst deleteProjectBtn = document.querySelector('#button-delete-project');\r\n// Add project - popup\r\nconst addProjectPopupBtn = document.querySelector('#add-project-popup');\r\nconst addPopupBtn = document.querySelector('#button-add-project-popup');\r\nconst cancelPopupBtn = document.querySelector('#button-cancel-project-popup');\r\n// Form & Input inside popup \r\nconst AddProjectForm = document.querySelector('#form-add-project');\r\nconst addProjectPopupInput = document.querySelector('#input-add-project-popup');\r\n\r\nconst openPopup = () => {\r\n    // Open pop-up view\r\n    addProjectBtn.classList.add('active');\r\n    deleteProjectBtn.classList.add('active');\r\n    addProjectPopupBtn.classList.add('active');\r\n    addPopupBtn.classList.add('active');\r\n    cancelPopupBtn.classList.add('active');\r\n}\r\n\r\nconst closePopup = () => {\r\n    // Close pop-up view\r\n    addProjectBtn.classList.remove('active');\r\n    deleteProjectBtn.classList.remove('active');\r\n    addProjectPopupBtn.classList.remove('active');\r\n    addPopupBtn.classList.remove('active');\r\n    cancelPopupBtn.classList.remove('active');\r\n}\r\n\r\naddProjectBtn.addEventListener('click', () => {\r\n    openPopup();\r\n    cancelPopupBtn.addEventListener('click', (e) => {\r\n        e.preventDefault()\r\n        closePopup();\r\n    })\r\n})\r\n\r\nconst clearElement = (element) => {\r\n    while (element.firstChild) {\r\n        element.removeChild(element.firstChild)\r\n    }\r\n}\r\n\r\nconst save = () => {\r\n    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));\r\n}\r\n\r\nconst render = () => {\r\n    clearElement(projectsList)\r\n    lists.forEach((list, index) => {\r\n        const newTitle = document.createElement('li');\r\n        newTitle.id = index;\r\n        newTitle.classList.add('title-name');\r\n        newTitle.innerText = list.title;\r\n        projectsList.appendChild(newTitle);\r\n    })\r\n}\r\n\r\nconst saveAndRender = () => {\r\n    save();\r\n    render();\r\n}\r\n\r\nconst createTitle = (title, index) => {\r\n    return {\r\n        id: title[index],\r\n        title: title,\r\n        tasks: []\r\n    }\r\n}\r\n\r\nAddProjectForm.addEventListener('submit', (e) => {\r\n    e.preventDefault()\r\n    const inputText = addProjectPopupInput.value;\r\n    console.log(inputText);\r\n    if (inputText === null || inputText === '') return\r\n    const list = createTitle(inputText);\r\n    addProjectPopupInput.value = null;\r\n    lists.push(list);\r\n    saveAndRender();\r\n})\r\n\r\nrender()\n\n//# sourceURL=webpack://todo-list/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,88 @@
-console.log('hello, test');
+const LOCAL_STORAGE_LIST_KEY = 'task.lists';
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+
+// New projects list
+const projectsList = document.querySelector('#projects-list');
+// Add/delete project - functionality
+const addProjectBtn = document.querySelector('#button-add-project');
+const deleteProjectBtn = document.querySelector('#button-delete-project');
+// Add project - popup
+const addProjectPopupBtn = document.querySelector('#add-project-popup');
+const addPopupBtn = document.querySelector('#button-add-project-popup');
+const cancelPopupBtn = document.querySelector('#button-cancel-project-popup');
+// Form & Input inside popup 
+const AddProjectForm = document.querySelector('#form-add-project');
+const addProjectPopupInput = document.querySelector('#input-add-project-popup');
+
+const openPopup = () => {
+    // Open pop-up view
+    addProjectBtn.classList.add('active');
+    deleteProjectBtn.classList.add('active');
+    addProjectPopupBtn.classList.add('active');
+    addPopupBtn.classList.add('active');
+    cancelPopupBtn.classList.add('active');
+}
+
+const closePopup = () => {
+    // Close pop-up view
+    addProjectBtn.classList.remove('active');
+    deleteProjectBtn.classList.remove('active');
+    addProjectPopupBtn.classList.remove('active');
+    addPopupBtn.classList.remove('active');
+    cancelPopupBtn.classList.remove('active');
+}
+
+addProjectBtn.addEventListener('click', () => {
+    openPopup();
+    cancelPopupBtn.addEventListener('click', (e) => {
+        e.preventDefault()
+        closePopup();
+    })
+})
+
+const clearElement = (element) => {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+}
+
+const save = () => {
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+}
+
+const render = () => {
+    clearElement(projectsList)
+    lists.forEach((list, index) => {
+        const newTitle = document.createElement('li');
+        newTitle.id = index;
+        newTitle.classList.add('title-name');
+        newTitle.innerText = list.title;
+        projectsList.appendChild(newTitle);
+    })
+}
+
+const saveAndRender = () => {
+    save();
+    render();
+}
+
+const createTitle = (title, index) => {
+    return {
+        id: title[index],
+        title: title,
+        tasks: []
+    }
+}
+
+AddProjectForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const inputText = addProjectPopupInput.value;
+    console.log(inputText);
+    if (inputText === null || inputText === '') return
+    const list = createTitle(inputText);
+    addProjectPopupInput.value = null;
+    lists.push(list);
+    saveAndRender();
+})
+
+render()
