5549da73343c90c47587a2e6dfbf388c3c2e4902
siIas <kaczkowski.lukasz@outlook.com>
2021-07-28 11:39:25 +0200

    adding tasks to projects

--- src/index.js
+++ src/index.js
@@ -21,6 +21,9 @@ const listDisplayContainer = document.querySelector('[data-list-display-containe
 const listTitleElement = document.querySelector('[data-list-title]');
 const listCountElement = document.querySelector('[data-list-count]');
 const tasksContainer = document.querySelector('[data-tasks]');
+const taskTemplate = document.querySelector('#task-template');
+const newTaskForm = document.querySelector('[data-new-task-form]');
+const newTaskInput = document.querySelector('[data-new-task-input]');
 
 const openPopup = () => {
     // Open pop-up view
@@ -56,18 +59,41 @@ const clearElement = (element) => {
 
 const save = () => {
     localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId);
+}
+
+const renderTaskCount = (selectedList) => {
+    const incompleteTasksCount = selectedList.tasks.filter(task => !task.complete).length;
+    const taskString = incompleteTasksCount === 1 ? "task" : "tasks";
+    listCountElement.innerText = `${incompleteTasksCount} ${taskString} remaining`;
+}
+
+const renderTasks = (selectedList) => {
+    selectedList.tasks.forEach(task => {
+        const taskElement = document.importNode(taskTemplate.content, true)
+        const checkbox = taskElement.querySelector('input')
+        checkbox.id = task.id
+        checkbox.checked = task.complete
+        const label = taskElement.querySelector('label')
+        label.htmlFor = task.id
+        label.append(task.title)
+        tasksContainer.appendChild(taskElement)
+    })
 }
 
 const render = () => {
-    clearElement(projectsList);
-    renderLists();
+    clearElement(projectsList)
+    renderLists()
 
-    const selectedList = lists.find(list => list.id === selectedListId);
+    const selectedList = lists.find(list => list.id === selectedListId)
     if (selectedListId === null) {
         listDisplayContainer.style.display = 'none';
     } else {
         listDisplayContainer.style.display = '';
         listTitleElement.innerText = selectedList.title;
+        renderTaskCount(selectedList)
+        clearElement(tasksContainer)
+        renderTasks(selectedList)
     }
 }
 
@@ -97,6 +123,14 @@ const createTitle = (title) => {
     }
 }
 
+const createTask = (title) => {
+    return {
+        id: Date.now().toString(),
+        title: title,
+        complete: false
+    }
+}
+
 AddProjectForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const inputText = addProjectPopupInput.value;
@@ -105,6 +139,17 @@ AddProjectForm.addEventListener('submit', (e) => {
     const list = createTitle(inputText);
     addProjectPopupInput.value = null;
     lists.push(list);
+    saveAndRender();
+})
+
+newTaskForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const taskName = newTaskInput.value;
+    if (taskName === null || taskName === '') return
+    const task = createTask(taskName);
+    newTaskInput.value = null;
+    const selectedList = lists.find(list => list.id === selectedListId);
+    selectedList.tasks.push(task);
     saveAndRender();
 })
 
