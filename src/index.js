bcfafe045d13066f0ff892c45063ff3f2dd8b941
siIas <kaczkowski.lukasz@outlook.com>
2021-07-31 00:58:39 +0200

    clear completed tasks function + important describing comments

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
 let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
 
-// New projects list
+// New projects list (lists container)
 const projectsList = document.querySelector('#projects-list');
-// Add/delete project - functionality
+// Add/delete/clear tasks project - functionality
 const addProjectBtn = document.querySelector('#button-add-project');
 const deleteProjectBtn = document.querySelector('#button-delete-project');
+const clearCompletedTasksBtn = document.querySelector('#button-clear-completed-tasks');
 // Add project - popup
 const addProjectPopupBtn = document.querySelector('#add-project-popup');
 const addPopupBtn = document.querySelector('#button-add-project-popup');
@@ -32,6 +33,7 @@ const openPopup = () => {
     addProjectPopupBtn.classList.add('active');
     addPopupBtn.classList.add('active');
     cancelPopupBtn.classList.add('active');
+    clearCompletedTasksBtn.classList.add('active');
 }
 
 const closePopup = () => {
@@ -41,9 +43,11 @@ const closePopup = () => {
     addProjectPopupBtn.classList.remove('active');
     addPopupBtn.classList.remove('active');
     cancelPopupBtn.classList.remove('active');
+    clearCompletedTasksBtn.classList.remove('active');
 }
 
 addProjectBtn.addEventListener('click', () => {
+    // Close/Open pop-up view
     openPopup();
     cancelPopupBtn.addEventListener('click', (e) => {
         e.preventDefault();
@@ -52,17 +56,21 @@ addProjectBtn.addEventListener('click', () => {
 })
 
 const clearElement = (element) => {
+    // Clear out all the elements that already exist in Projects List
+    // Result is; all those existing elements are removed, only adds a new ones
     while (element.firstChild) {
         element.removeChild(element.firstChild);
     }
 }
 
 const save = () => {
+    // Save function is responsible for saving items&currently choosed elements to localStorage 
     localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
-    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId);
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId); // actually clicked element
 }
 
 const renderTaskCount = (selectedList) => {
+    // Display amount of remaining tasks
     const incompleteTasksCount = selectedList.tasks.filter(task => !task.complete).length;
     const taskString = incompleteTasksCount === 1 ? "task" : "tasks";
     listCountElement.innerText = `${incompleteTasksCount} ${taskString} remaining`;
@@ -70,14 +78,30 @@ const renderTaskCount = (selectedList) => {
 
 const renderTasks = (selectedList) => {
     selectedList.tasks.forEach(task => {
-        const taskElement = document.importNode(taskTemplate.content, true)
+        // Rendering(adding) new tasks using template pattern made in html
+        const taskElement = document.importNode(taskTemplate.content, true); // true to render everything inside template(not just top of it)
         const checkbox = taskElement.querySelector('input')
-        checkbox.id = task.id
-        checkbox.checked = task.complete
+        checkbox.id = task.id // checkbox got same id as task element
+        checkbox.checked = task.complete // if task is checked = it's marked as complete
         const label = taskElement.querySelector('label')
         label.htmlFor = task.id
         label.append(task.title)
         tasksContainer.appendChild(taskElement)
+    })
+}
+
+const renderLists = () => {
+    // Rendering(adding) new Project Lists on the page
+    lists.forEach((list) => {
+        const newTitle = document.createElement('li');
+        newTitle.dataset.listId = list.id;
+        newTitle.classList.add('title-name');
+        newTitle.innerText = list.title;
+        // #1 list.id === selectedListId
+        if (list.id === selectedListId) {
+            newTitle.classList.add('active-list');
+        }
+        projectsList.appendChild(newTitle);
     })
 }
 
@@ -87,6 +111,7 @@ const render = () => {
 
     const selectedList = lists.find(list => list.id === selectedListId)
     if (selectedListId === null) {
+        // If Projects List is not selected - hide add task section
         listDisplayContainer.style.display = 'none';
     } else {
         listDisplayContainer.style.display = '';
@@ -95,19 +120,6 @@ const render = () => {
         clearElement(tasksContainer)
         renderTasks(selectedList)
     }
-}
-
-const renderLists = () => {
-    lists.forEach((list) => {
-        const newTitle = document.createElement('li');
-        newTitle.dataset.listId = list.id;
-        newTitle.classList.add('title-name');
-        newTitle.innerText = list.title;
-        if (list.id === selectedListId) {
-            newTitle.classList.add('active-list');
-        }
-        projectsList.appendChild(newTitle);
-    })
 }
 
 const saveAndRender = () => {
@@ -132,9 +144,9 @@ const createTask = (title) => {
 }
 
 AddProjectForm.addEventListener('submit', (e) => {
+    // Creating new List Project based on submit input values
     e.preventDefault();
     const inputText = addProjectPopupInput.value;
-    console.log(inputText);
     if (inputText === null || inputText === '') return
     const list = createTitle(inputText);
     addProjectPopupInput.value = null;
@@ -148,21 +160,40 @@ newTaskForm.addEventListener('submit', e => {
     if (taskName === null || taskName === '') return
     const task = createTask(taskName);
     newTaskInput.value = null;
-    const selectedList = lists.find(list => list.id === selectedListId);
-    selectedList.tasks.push(task);
+    const selectedList = lists.find(list => list.id === selectedListId); // list that is currently selected
+    selectedList.tasks.push(task); // add a new task to currently selected list
     saveAndRender();
 })
 
 projectsList.addEventListener('click', e => {
+    // Extension to renderLists function, making clicked element on Projects List active
     if (e.target.tagName.toLowerCase() === 'li') {
+        // #2 list.id === selectedListId
         selectedListId = e.target.dataset.listId;
         saveAndRender();
+    }
+})
+
+tasksContainer.addEventListener('click', e => {
+    // Making selected(clicked) tasks active/inactive
+    if (e.target.tagName.toLowerCase() === 'input') { // check if clicked on Checkbox
+        const selectedList = lists.find(list => list.id === selectedListId);
+        const selectedTask = selectedList.tasks.find(task => task.id === e.target.id); // comparate task.id to Checkbox id(e.target.id)
+        selectedTask.complete = e.target.checked;
+        save();
+        renderTaskCount(selectedList);
     }
 })
 
 deleteProjectBtn.addEventListener('click', () => {
     lists = lists.filter(list => list.id !== selectedListId);
     selectedListId = null;
+    saveAndRender();
+})
+
+clearCompletedTasksBtn.addEventListener('click', () => {
+    const selectedList = lists.find(list => list.id === selectedListId); // choose actually selected list
+    selectedList.tasks = selectedList.tasks.filter(task => !task.complete); // overwrite tasks, so basically keep tasks that are not completed
     saveAndRender();
 })
 
